ebc526a9d71f875ce24a71a2f4bd1e060ad9c3ce
pupu08429-svg <pupu08429@gmail.com>
2026-04-19 22:10:24 +0300

    tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,42 +1,46 @@
-// Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-#include <cassert>
-
-#include <iostream>
-
 template<typename T, int Nsize>
 class TStack {
-private:
-  T data[Nsize];
-  int topIndex;
+   private:
+    T data[Nsize];
+    int topIndex;
 
-public:
-  TStack() : topIndex(-1) {}
-  bool isEmpty() const {
-    return topIndex == -1;
-  }
-  bool isFull() const {
-    return topIndex == Nsize - 1;
-  }
-  void push(const T& value) {
-    if (!isFull()) {
-      data[++topIndex] = value;
-  }
-  void pop() {
-    if (!isEmpty()) {
-    --topIndex;
-  }
-  T top() const {
-    return data[topIndex];
-  }
-  T pop() {
-    return data[topIndex--];
-  }
-  int getsize() const {
-    return topIndex + 1;
-  }
+   public:
+    TStack() : topIndex(-1) {}
+
+    bool isEmpty() const {
+      return topIndex == -1;
+    }
+
+    bool isFull() const {
+      return topIndex == Nsize - 1;
+    }
+
+    void push(const T& value) {
+      if (!isFull()) {
+        data[++topIndex] = value;
+      }
+    }
+
+    void pop() {
+      if (!isEmpty()) {
+        --topIndex;
+      }
+    }
+
+    T top() const {
+      return data[topIndex];
+    }
+
+    T popAndGet() {
+      return data[topIndex--];
+    }
+
+    int getSize() const {
+      return topIndex + 1;
+    }
 };
 
 #endif
