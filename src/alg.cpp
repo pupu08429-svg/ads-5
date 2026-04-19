5784cf976149d7106b29a727b5d2e0796e4288b4
pupu08429-svg <pupu08429@gmail.com>
2026-04-19 22:16:45 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,98 +1,107 @@
-// Copyright 2025 NNTU-CS
+#include <map>
 #include "tstack.h"
 
-#include <map>
-#include <string>
-
-int getPriority(char op) {
-  std::map<char, int> priority = {
-    {'(', 0}, {')', 1}, {'+', 2}, {'-', 2}, {'*', 3}, {'/', 3}
-  };
-  return priority[op];
+int checkPr(const char ch) {
+  if (ch == '(')
+    return 0;
+  else if (ch == ')')
+    return 1;
+  else if (ch == '+' || ch == '-')
+    return 2;
+  else if (ch == '*' || ch == '/')
+    return 3;
+  else
+    throw "Incorrect character";
 }
 
-std::string infx2pstfx(std::string inf) {
-  std::string output;
-  TStack<char, 100> stack;
-
-  for (size_t i = 0; i < inf.length(); i++) {
-    char current = inf[i];
-
-    if (current >= '0' && current <= '9') {
-      while (i < inf.length() && inf[i] >= '0' && inf[i] <= '9') {
-        output += inf[i];
+std::string infx2pstfx(const std::string& inf) {
+  TStack<char, 100> stack1;
+  int i = 0;
+  std::string res;
+  while (i < inf.length()) {
+    if (inf[i] >= '0' && inf[i] <= '9') {
+      while (inf[i] >= '0' && inf[i] <= '9') {
+        res += inf[i];
         i++;
       }
-      output += ' ';
-      i--;
-    } else if (current == '(') {
-      stack.push(current);
-    } else if (current == ')') {
-      while (!stack.isEmpty() && stack.top() != '(') {
-        output += stack.top();
-        output += ' ';
-        stack.pop();
+      res += ' ';
+      continue;
       }
-      if (!stack.isEmpty() && stack.top() == '(') {
-        stack.pop();
+    if (checkPr(inf[i]) == 0) {
+      stack1.push(inf[i]);
+    } else if (checkPr(inf[i]) == 1) {
+      while (!stack1.isEmpty() && checkPr(stack1.get()) != 0) {
+        res += stack1.get();
+        res += ' ';
+        stack1.pop();
       }
-    } else if (current == '+' || current == '-' ||
-               current == '*' || current == '/') {
-      while (!stack.isEmpty() && stack.top() != '(' &&
-             getPriority(stack.top()) >= getPriority(current)) {
-        output += stack.top();
-        output += ' ';
-        stack.pop();
+      stack1.pop();
+    } else if (checkPr(inf[i]) > 1) {
+      if (stack1.isEmpty() || checkPr(inf[i]) > checkPr(stack1.get())) {
+        stack1.push(inf[i]);
+      } else {
+        while (!stack1.isEmpty() &&
+          stack1.get() != '('
+          && checkPr(inf[i]) <= checkPr(stack1.get())) {
+          res += stack1.get();
+          res += ' ';
+          stack1.pop();
+        }
+        stack1.push(inf[i]);
       }
-      stack.push(current);
     }
+    i++;
   }
-
-  while (!stack.isEmpty()) {
-    output += stack.top();
-    if (stack.getSize() != 1) {
-      output += ' ';
-    }
-    stack.pop();
+  while (!stack1.isEmpty()) {
+    res += stack1.get();
+    if (stack1.getSize() != 1)
+      res += ' ';
+    stack1.pop();
   }
-
-  return output;
+  return res;
 }
 
-int eval(std::string post) {
-  TStack<int, 100> stack;
-
-  for (size_t i = 0; i < post.length(); i++) {
-    char current = post[i];
-
-    if (current >= '0' && current <= '9') {
-      int number = 0;
-      while (i < post.length() && post[i] >= '0' && post[i] <= '9') {
-        number = number * 10 + (post[i] - '0');
+int eval(const std::string& pref) {
+  TStack<int, 100> stack2;
+  int res = 0, i = 0;
+  while (i < pref.length()) {
+    if (pref[i] >= '0' && pref[i] <= '9') {
+      std::string num;
+      while (pref[i] != ' ') {
+        num += pref[i];
         i++;
       }
-      stack.push(number);
-      i--;
-    } else if (current == '+' || current == '-' ||
-               current == '*' || current == '/') {
-      int b = stack.top();
-      stack.pop();
-      int a = stack.top();
-      stack.pop();
-      int result = 0;
-
-      if (current == '+') {
-        result = a + b;
-      } else if (current == '-') {
-        result = a - b;
-      } else if (current == '*') {
-        result = a * b;
-      } else if (current == '/') {
-        result = a / b;
-      }
-
-      stack.push(result);
+      i++;
+      stack2.push(std::stoi(num));
+      continue;
     }
+    if (pref[i] == '+') {
+      res = stack2.get();
+      stack2.pop();
+      res += stack2.get();
+      stack2.pop();
+      stack2.push(res);
+    } else if (pref[i] == '-') {
+      res = stack2.get();
+      stack2.pop();
+      res = stack2.get() - res;
+      stack2.pop();
+      stack2.push(res);
+    } else if (pref[i] == '*') {
+      res = stack2.get();
+      stack2.pop();
+      res *= stack2.get();
+      stack2.pop();
+      stack2.push(res);
+    } else if (pref[i] == '/') {
+      res = stack2.get();
+      stack2.pop();
+      res = stack2.get() / res;
+      stack2.pop();
+      stack2.push(res);
+    }
+    res = 0;
+    i += 2;
   }
-  return stack.top();
+  return stack2.get();
 }
