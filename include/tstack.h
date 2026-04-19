c8ae511923f5772aa593de70938e89ebbd356f84
pupu08429-svg <pupu08429@gmail.com>
2026-04-19 21:07:21 +0300

    tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,43 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <cassert>
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+  T data[N];
+  int topIndex;
+public:
+  TStack() : topIndex(-1) {}
+  bool isEmpty() const {
+    return topIndex == -1;
+  }
+  bool isFull() const {
+    return topIndex == N - 1;
+  }
+  void push(const T& value) {
+    assert(!isFull() && "Stack overflow!");
+    data[++topIndex] = value;
+  }
+  void pop() {
+    assert(!isEmpty() && "Stack underflow!");
+    --topIndex;
+  }
+  T top() const {
+    assert(!isEmpty() && "Stack is empty!");
+    return data[topIndex];
+  }
+  T popAndGet() {
+    assert(!isEmpty() && "Stack underflow!");
+    return data[topIndex--];
+  }
+  void clear() {
+    topIndex = -1;
+  }
+  int size() const {
+    return topIndex + 1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
