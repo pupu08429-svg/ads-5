32bffd3599a64f0d32907b17eefc603be87402c0
pupu08429-svg <pupu08429@gmail.com>
2026-04-19 21:16:51 +0300

    tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -5,39 +5,40 @@
 
 template<typename T, int size>
 class TStack {
-private:
-  T data[N];
-  int topIndex;
-public:
-  TStack() : topIndex(-1) {}
-  bool isEmpty() const {
-    return topIndex == -1;
-  }
-  bool isFull() const {
-    return topIndex == N - 1;
-  }
-  void push(const T& value) {
-    assert(!isFull() && "Stack overflow!");
-    data[++topIndex] = value;
-  }
-  void pop() {
-    assert(!isEmpty() && "Stack underflow!");
-    --topIndex;
-  }
-  T top() const {
-    assert(!isEmpty() && "Stack is empty!");
-    return data[topIndex];
-  }
-  T popAndGet() {
-    assert(!isEmpty() && "Stack underflow!");
-    return data[topIndex--];
-  }
-  void clear() {
-    topIndex = -1;
-  }
-  int size() const {
-    return topIndex + 1;
-  }
+  private:
+    T data[N];
+    int topIndex;
+
+  public:
+    TStack() : topIndex(-1) {}
+    bool isEmpty() const {
+      return topIndex == -1;
+    }
+    bool isFull() const {
+      return topIndex == N - 1;
+    }
+    void push(const T& value) {
+      assert(!isFull() && "Stack overflow!");
+      data[++topIndex] = value;
+    }
+    void pop() {
+      assert(!isEmpty() && "Stack underflow!");
+      --topIndex;
+    }
+    T top() const {
+      assert(!isEmpty() && "Stack is empty!");
+      return data[topIndex];
+    }
+    T popAndGet() {
+      assert(!isEmpty() && "Stack underflow!");
+      return data[topIndex--];
+    }
+    void clear() {
+      topIndex = -1;
+    }
+    int size() const {
+      return topIndex + 1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
